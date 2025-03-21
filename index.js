16b1884f231c7e058921af2828ba41f54d2ccd37
Nezareeen <nezareeen@gmail.com>
2025-03-21 14:09:10 +0530

    Complete

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require('express');
+const mongoose = require("mongoose");
+require("dotenv").config();
+const mySchema = require("./schema");
 const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
 
+
+app.use(express.json());
 app.use(express.static('static'));
+
+
+
+async function connectDataBase(){
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to database")
+  } catch (error) {
+    console.log("Error connecting to database");
+  }
+};
+
+connectDataBase();
+
+
+app.post('/api/users',async (req,res)=>{
+  try{
+    console.log(req.body)
+  const schema = new mySchema(req.body);
+  const savedUser = await schema.save();
+  res.status(201).send({msg:'user created ',data:savedUser})
+}catch(err){
+  res.status(500).send({msg:'something went wrong',err})
+}
+
+
+})
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
